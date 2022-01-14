9b63ccacdfea083f33f07fa5c7960024ce78d4c7
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-15 00:58:51 +0700

    Set up react-moralis

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import { MoralisProvider } from 'react-moralis';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
