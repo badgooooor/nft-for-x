581abc5e62e87d25f8659762e3974345009287bf
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-15 22:34:18 +0700

    Add react-parallax-tilt

--- components/campaign/NFTRedeemCard.jsx
+++ components/campaign/NFTRedeemCard.jsx
@@ -1,21 +1,23 @@
-import Tilt from "react-parallax-tilt";
+import Tilt from 'react-parallax-tilt';
 
-const NFTRedeemCard = ({ tokenId, redeem }) => {
+const NFTRedeemCard = ({ tokenId, imgSrc, openRedeem }) => {
   return (
     <Tilt>
-      <div className="max-w-sm rounded-sm shadow-lg bg-white">
-        <img
-          className="w-full rounded-t-sm"
-          src="https://media.discordapp.net/attachments/929752477711073340/931745821056176168/tshirt.jpg"
-          alt="NFT token for redeem"
-        />
-        <div className="px-6 py-4">
-          <div className="font-semibold text-xl mb-2">Token #{tokenId}</div>
+      <div className='max-w-sm rounded-sm shadow-lg bg-white'>
+        <div>
+          <img
+            className='w-full h-72 object-cover rounded-t-sm'
+            src={imgSrc}
+            alt='NFT token for redeem'
+          />
         </div>
-        <div className="px-6 py-4 text-center">
+        <div className='px-6 py-4'>
+          <div className='font-semibold text-xl mb-2'>Token #{tokenId}</div>
+        </div>
+        <div className='px-6 py-4 text-center'>
           <button
-            className="w-full ease-in duration-300 bg-dark-slate-blue hover:bg-white text-white hover:text-dark-slate-blue border-2 border-dark-slate-blue rounded-md px-8 py-2"
-            onClick={() => redeem(tokenId)}
+            className='w-full ease-in duration-300 bg-dark-slate-blue hover:bg-white text-white hover:text-dark-slate-blue border-2 border-dark-slate-blue rounded-md px-8 py-2'
+            onClick={() => openRedeem(tokenId)}
           >
             Redeem
           </button>
