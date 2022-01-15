7a9bf0f60da7ff512a80406bf367851e82dcc7b0
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-15 14:55:19 +0700

    Add anchor tag for redirecting to home page

--- components/common/NavBar.jsx
+++ components/common/NavBar.jsx
@@ -3,10 +3,12 @@ import Account from './Account';
 const NavBar = () => {
   return (
     <nav className='flex flex-wrap items-center justify-between p-4 bg-[#b2d7df]'>
-      <div className='flex items-center mr-6 text-black flex-no-shrink'>
-        <span className='font-serif text-xl font-semibold tracking-tight text-[#feff79]'>
-          NFTForX
-        </span>
+      <div className='flex items-center mr-6 text-black flex-no-shrink px-6'>
+        <a href='/'>
+          <span className='font-serif text-3xl font-semibold tracking-tight text-black'>
+            NFTForX
+          </span>
+        </a>
       </div>
       <div className='flex items-end'>
         <Account />
