797b1d4e8847759881b93dacbde0a402e325f941
Tantatorn <timefear007@gmail.com>
2022-01-15 11:31:36 +0700

    add ong ang card

--- components/common/NavBar.jsx
+++ components/common/NavBar.jsx
@@ -1,11 +1,11 @@
 const NavBar = () => {
   return (
     <nav className="flex flex-wrap items-center justify-between p-4 bg-[#b2d7df]">
-        <div className="flex items-center mr-6 text-black flex-no-shrink">
-          <span className="font-serif text-xl font-semibold tracking-tight text-[#feff79]">
-            NFTForX
-          </span>
-        </div>
+      <div className="flex items-center mr-6 text-black flex-no-shrink">
+        <span className="font-serif text-xl font-semibold tracking-tight text-[#feff79]">
+          NFTForX
+        </span>
+      </div>
     </nav>
   );
 };
