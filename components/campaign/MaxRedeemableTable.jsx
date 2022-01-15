7e8312e1f01cf1c59d771965550a25e50bb87976
Tantatorn <timefear007@gmail.com>
2022-01-15 14:21:36 +0700

    add input for max redeemable amount

--- components/campaign/MaxRedeemableTable.jsx
+++ components/campaign/MaxRedeemableTable.jsx
@@ -1,10 +1,28 @@
 import { useRecoilState } from "recoil";
 import { campaignOptionState } from "../../lib/states";
 
-const MaxRedeemableTable = () => {
+const OptionAmountInput = ({ name }) => {
   const [campaign, setCampaign] = useRecoilState(campaignOptionState);
+
   return (
-    <table className="table-fixed">
+    <input
+      onChange={(e) => {
+        let newCampaign = { ...campaign };
+        newCampaign[name] = { maxRedeemableAmount: e.target.value };
+        setCampaign(newCampaign);
+      }}
+      value={campaign[name].maxRedeemableAmount}
+      type="number"
+      id="t_shirt_max_amount"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      required
+    />
+  );
+};
+
+const MaxRedeemableTable = () => {
+  return (
+    <table className="max-w-sm table-fixed">
       <thead>
         <tr>
           <th>Item</th>
@@ -14,15 +32,15 @@ const MaxRedeemableTable = () => {
       <tbody className="text-center">
         <tr>
           <td>T Shirt</td>
-          <td>{campaign["TShirt"].maxRedeemableAmount}</td>
+          <OptionAmountInput name="TShirt" />
         </tr>
         <tr>
           <td>Hat</td>
-          <td>{campaign["Hat"].maxRedeemableAmount}</td>
+          <OptionAmountInput name="Hat" />
         </tr>
         <tr>
           <td>Jacket</td>
-          <td>{campaign["Jacket"].maxRedeemableAmount}</td>
+          <OptionAmountInput name="Jacket" />
         </tr>
       </tbody>
     </table>
