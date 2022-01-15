9b76d24d2fe4e7ea2fc2caacb8e943c8021612e3
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-15 15:42:47 +0700

    Add show NFTForX address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,22 +3,22 @@
 async function main() {
   const [nftCreator] = await ethers.getSigners();
   // Mock NFT
-  const MockNFT = await ethers.getContractFactory("MockNFT");
+  const MockNFT = await ethers.getContractFactory('MockNFT');
   const mockNFT = await MockNFT.deploy();
   await mockNFT.deployed();
 
-  console.log("Mock NFT deployed at: " + mockNFT.address);
+  console.log('Mock NFT deployed at: ' + mockNFT.address);
 
   // TODO: Put address here!
   // Mint NFT
-  await mockNFT.mint(nftCreator.address);
+  await mockNFT.mintTo(nftCreator.address, 10);
 
   // NFTForXFactory
-  const NFTForXFactory = await ethers.getContractFactory("NFTForXFactory");
+  const NFTForXFactory = await ethers.getContractFactory('NFTForXFactory');
   const nftForXFactory = await NFTForXFactory.deploy();
   await nftForXFactory.deployed();
 
-  console.log("NFTForX Factory deployed at: " + nftForXFactory.address);
+  console.log('NFTForX Factory deployed at: ' + nftForXFactory.address);
 
   // Create a new campaign
   // Params:
@@ -26,8 +26,10 @@ async function main() {
   // - collection: address of the collection of NFT
   // - maxRedeemPerUser: maximum number of NFT that can be redeemed per user
   await nftForXFactory.createCampaign(nftCreator.address, mockNFT.address, 20); // => this will Emit NFTForXCreated event
-}
 
+  const nftForX = await nftForXFactory.getCampaign(0);
+  console.log('NFTForX deployed at: ' + nftForX);
+}
 
 main()
   .then(() => process.exit(0))
