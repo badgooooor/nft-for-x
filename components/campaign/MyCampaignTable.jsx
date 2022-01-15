a5596e220f517e2f5ccc934b07adbd32489510d5
Tantatorn <timefear007@gmail.com>
2022-01-15 15:33:16 +0700

    use Recoil state for store my campaign data

--- components/campaign/MyCampaignTable.jsx
+++ components/campaign/MyCampaignTable.jsx
@@ -1,4 +1,47 @@
+import { useRecoilState } from "recoil";
+import { myCampaignState } from "../../lib/states";
+
+const Row = ({ campaignName, redeemCount, items, nft }) => {
+  return (
+    <tr>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="flex-shrink-0 w-10 h-10"></div>
+          <div className="ml-4">
+            <div className="text-sm font-medium text-gray-900">
+              {campaignName}
+            </div>
+          </div>
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">{redeemCount}</td>
+      <td className="px-6 py-4 space-x-2 text-sm whitespace-nowrap">
+        <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#f4c9e7] rounded-full">
+          T Shirt x {items["tShirt"]}
+        </span>
+        <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#f4c9e7] rounded-full">
+          Hat x {items["hat"]}
+        </span>
+        <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#f4c9e7] rounded-full">
+          Jacket x {items["jacket"]}
+        </span>
+      </td>
+      <td className="px-6 py-4 space-x-2 text-sm whitespace-nowrap">
+        {nft.map((i) => (
+          <span
+            key={i}
+            className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#feff79] rounded-full"
+          >
+            {i}
+          </span>
+        ))}
+      </td>
+    </tr>
+  );
+};
+
 const MyCampaignTable = () => {
+  const [campaigns] = useRecoilState(myCampaignState);
   return (
     <div className="flex flex-col max-w-3xl">
       <div className="-my-2 sm:-mx-6 lg:-mx-8">
@@ -37,38 +80,17 @@ const MyCampaignTable = () => {
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
-                <tr>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 w-10 h-10"></div>
-                      <div className="ml-4">
-                        <div className="text-sm font-medium text-gray-900">
-                          {"Ong Ang's Winter Collection"}
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">100</td>
-                  <td className="px-6 py-4 space-x-2 text-sm whitespace-nowrap">
-                    <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#f4c9e7] rounded-full">
-                      T Shirt x 0
-                    </span>
-                    <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#f4c9e7] rounded-full">
-                      Hat x 0
-                    </span>
-                    <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#f4c9e7] rounded-full">
-                      Jacket x 0
-                    </span>
-                  </td>
-                  <td className="px-6 py-4 space-x-2 text-sm whitespace-nowrap">
-                    <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#feff79] rounded-full">
-                      0x122324325432
-                    </span>
-                    <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-[#feff79] rounded-full">
-                      0x122324325a32
-                    </span>
-                  </td>
-                </tr>
+                {campaigns.map((campaign) => {
+                  return (
+                    <Row
+                      key={campaign}
+                      campaignName={campaign.campaignName}
+                      redeemCount={campaign.redeemCount}
+                      items={campaign.items}
+                      nft={campaign.nft}
+                    />
+                  );
+                })}
               </tbody>
             </table>
           </div>
