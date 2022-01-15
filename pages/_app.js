a83aefd47a592b0a889467ae546ce784dd5501b4
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-15 10:57:34 +0700

    Add NavBar

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 
 import { MoralisProvider } from 'react-moralis';
 
+import NavBar from '../components/common/NavBar';
+
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
       appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
       serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
     >
+      <NavBar />
       <Component {...pageProps} />
     </MoralisProvider>
   );
