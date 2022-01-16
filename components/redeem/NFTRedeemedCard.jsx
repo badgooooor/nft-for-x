68af2686aa880ab935469e17b11b3b791bfe30af
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-16 16:23:32 +0700

    Edit redeem style

--- components/redeem/NFTRedeemedCard.jsx
+++ components/redeem/NFTRedeemedCard.jsx
@@ -1,19 +1,36 @@
 import Tilt from 'react-parallax-tilt';
 
-const NFTRedeemedCard = ({ tokenId, optionId, imgSrc }) => {
+const NFTRedeemedCard = ({ tokenId, optionId }) => {
+  const imageData = {
+    0: {
+      src: '/collection/tshirt.jpg',
+      name: 'T Shirt',
+      description: 'Shirt engraved with awesomeness',
+    },
+    1: {
+      src: '/collection/hat.jpg',
+      name: 'Hat',
+      description: 'Hat showing how sultan you are',
+    },
+    2: {
+      src: '/collection/jacket.jpg',
+      name: 'Jacket',
+      description: 'Jacket that make you drip like rockstar',
+    },
+  };
   return (
     <Tilt>
-      <div className='max-w-sm rounded-sm shadow-lg'>
+      <div className='max-w-sm rounded-md shadow-lg bg-white'>
         <div>
           <img
             className='w-full h-72 object-cover rounded-t-sm'
-            src={imgSrc}
+            src={imageData[optionId].src}
             alt='NFT token for redeem'
           />
         </div>
         <div className='px-6 py-4'>
-          <div className='font-semibold text-xl mb-1'>Token #{tokenId}</div>
-          <div className='font-medium text-md mb-2'>Option #{optionId}</div>
+          <div className='font-semibold text-xl mb-1'>{imageData[optionId].name}</div>
+          <div className='font-medium text-md mb-2'>Redeemed with token #{tokenId}</div>
         </div>
       </div>
     </Tilt>
--- components/redeem/UserTokenContainer.jsx
+++ components/redeem/UserTokenContainer.jsx
@@ -1,9 +1,5 @@
 const UserTokenContainer = ({ children }) => {
-  return (
-    <div className='grid sm:grid-cols-4 grid-cols-2 gap-4 bg-white-pink rounded border-light-pink p-4'>
-      {children}
-    </div>
-  );
+  return <div className='grid sm:grid-cols-4 grid-cols-2 gap-4'>{children}</div>;
 };
 
 export default UserTokenContainer;
