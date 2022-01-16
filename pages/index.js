8ee14901c65fcd1f053970659280b14dec4f1e16
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-16 13:34:56 +0700

    Style campaign title

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import MintMockButton from "../components/common/MintMockButton";
-import CampaignList from "../components/landing/CampaignList";
+import MintMockButton from '../components/common/MintMockButton';
+import CampaignList from '../components/landing/CampaignList';
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-center">NFT4X</h1>
-      <div className="flex items-center justify-between mb-4">
-        <div className="text-xl text-left">Campaigns</div>
+      <h1 className='text-3xl font-bold text-center'>NFT4X</h1>
+      <div className='flex items-center justify-between mb-4'>
+        <div className='font-bold text-2xl mb-2'>Campaigns</div>
         <MintMockButton />
       </div>
       <CampaignList />
