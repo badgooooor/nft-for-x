228707e6fd65f4197b1258bcaa33243444800e35
Tantatorn <timefear007@gmail.com>
2022-01-16 22:37:19 +0700

    add mumbai banner

--- components/common/Layout.jsx
+++ components/common/Layout.jsx
@@ -1,18 +1,36 @@
-import Head from 'next/head';
-import NavBar from './NavBar';
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import Banner from "./Banner";
+import NavBar from "./NavBar";
 
 const Layout = ({ children }) => {
+  const [isMumbaiShowed, setIsMumbaiShowed] = useState(false);
+
+  useEffect(() => {
+    const mumbaiShowed = localStorage.getItem("mumbaiShowed");
+    if (mumbaiShowed == "yes") {
+      setIsMumbaiShowed(true);
+    } else {
+      setIsMumbaiShowed(false);
+    }
+  }, []);
+
   return (
     <div>
       <Head>
         <title>NFT for X</title>
-        <meta name='description' content='Best NFT Hack Application' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Best NFT Hack Application" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div className="relative">
+        {!isMumbaiShowed && (
+          <div className="flex justify-center">
+            <Banner />
+          </div>
+        )}
         <NavBar />
-        <div className='flex justify-center'>
-          <div className='w-5/6 mx-4 pt-4'>{children}</div>
+        <div className="flex justify-center">
+          <div className="w-5/6 pt-4 mx-4">{children}</div>
         </div>
       </div>
     </div>
