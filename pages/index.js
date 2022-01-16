927b7d4ed1b5e35c290bf1b8220ee2ac86ee9776
Tantatorn <timefear007@gmail.com>
2022-01-16 13:36:36 +0700

    add logo to home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
+import Tilt from "react-parallax-tilt";
 import Image from "next/image";
 import CampaignCard from "../components/campaign/CampaignCard";
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-center">NFT4X</h1>
+      <div className="text-center">
+        <Tilt>
+          <Image
+            src={"/logo/brand-full.png"}
+            alt="full-logo"
+            height={120}
+            width={390}
+          />
+        </Tilt>
+      </div>
       <h1 className="m-8 text-xl text-left">Campaigns</h1>
       <CampaignCard />
     </div>
