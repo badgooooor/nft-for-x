a90f6a1bbb2ef39b744ae5d71394fc28aae79bda
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-01-16 12:37:31 +0700

    Add state pathName

--- components/common/SwitchButton.jsx
+++ components/common/SwitchButton.jsx
@@ -1,12 +1,18 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const SwitchButton = () => {
   const router = useRouter();
+  const [pathName, setPathName] = useState();
+
+  useEffect(() => {
+    setPathName(router.pathname);
+  }, [router.pathname]);
 
   return (
     <div className='mr-2'>
-      {router.pathname !== '/' ? (
+      {pathName !== '/' ? (
         <button className='ease-in duration-300 bg-blue-500 hover:bg-white text-white text-sm hover:text-blue-500 border-2 border-blue-500 rounded-md px-4 py-2'>
           <Link href={`/`}>Switch to redeemer</Link>
         </button>
